d2f8f2e922512970a83d1570bde4fa1efd877efb
Darko Tasevski <c.dtasevski@gogoair.com>
2018-09-27 17:45:57 +0200

    feat: Add company data

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,30 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 const axios = require('axios');
 
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+	},
+});
+
 const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			async resolve(parentValue, args) {
+				const { data } = await axios.get(
+					`http://localhost:3000/companies/${parentValue.companyId}`
+				);
+				return data;
+			},
+		},
 	},
 });
 
