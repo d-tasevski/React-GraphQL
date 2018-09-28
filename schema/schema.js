1b9c246d042f190bbe1ea871068e57102700da47
Darko Tasevski <c.dtasevski@gogoair.com>
2018-09-28 21:54:21 +0200

    update: Schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,33 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList,
+} = require('graphql');
 const axios = require('axios');
 
 const CompanyType = new GraphQLObjectType({
 	name: 'Company',
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
 		description: { type: GraphQLString },
-	},
+		users: {
+			type: new GraphQLList(UserType),
+			async resolve(parentValue, args) {
+				const { data } = await axios.get(
+					`http://localhost:3000/companies/${parentValue.id}/users`
+				);
+				return data;
+			},
+		},
+	}),
 });
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
@@ -25,7 +40,7 @@ const UserType = new GraphQLObjectType({
 				return data;
 			},
 		},
-	},
+	}),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -38,6 +53,29 @@ const RootQuery = new GraphQLObjectType({
 				const { data } = await axios.get(`http://localhost:3000/users/${args.id}`);
 				return data;
 			},
+		},
+		company: {
+			type: CompanyType,
+			args: { id: { type: GraphQLString } },
+			async resolve(parentValue, args) {
+				const { data } = await axios.get(`http://localhost:3000/companies/${args.id}`);
+				return data;
+			},
+		},
+	},
+});
+
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			type: UserType,
+			args: {
+				firstName: { type: GraphQLString },
+				age: { type: GraphQLInt },
+				companyId: { type: GraphQLString },
+			},
+			resolve() {},
 		},
 	},
 });
